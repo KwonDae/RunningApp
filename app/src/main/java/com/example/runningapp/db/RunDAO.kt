edfad594d144c76bd784868033a466c6e60e4296
daewon.kwon <dae_won.kwon@kt.com>
2021-12-13 17:49:48 +0900

    #2 Run Entity & RunDao complete

--- app/src/main/java/com/example/runningapp/db/RunDAO.kt
+++ app/src/main/java/com/example/runningapp/db/RunDAO.kt
@@ -26,4 +26,16 @@ interface RunDAO {
 
     @Query("SELECT * FROM running_table ORDER BY distanceInMeters DESC")
     fun getAllRunsSortedByDistance(): LiveData<List<Run>>
+
+    @Query("SELECT SUM(caloriesBurned) FROM running_table")
+    fun getTotalTimeInMillis(): LiveData<Long>
+
+    @Query("SELECT SUM(timeInMillis) FROM running_table")
+    fun getTotalCaloriesBurned(): LiveData<Int>
+
+    @Query("SELECT SUM(distanceInMeters) FROM running_table")
+    fun getTotalDistance(): LiveData<Int>
+
+    @Query("SELECT SUM(avgSpeedInKMH) FROM running_table")
+    fun getTotalAvgSpeed(): LiveData<Float>
 }
