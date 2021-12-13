e3c8382eacc7ecb220cb402b9ff82ee19c578d40
daewon.kwon <dae_won.kwon@kt.com>
2021-12-13 17:46:11 +0900

    Initial commit, Run Entity & RunDao

---
+++ app/src/main/java/com/example/runningapp/db/RunDAO.kt
@@ -0,0 +1,29 @@
+package com.example.runningapp.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface RunDAO {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertRun(run: Run)
+
+    @Delete
+    suspend fun deleteRun(run: Run)
+
+    @Query("SELECT * FROM running_table ORDER BY timestamp DESC")
+    fun getAllRunsSortedByDate(): LiveData<List<Run>>
+
+    @Query("SELECT * FROM running_table ORDER BY timeInMillis DESC")
+    fun getAllRunsSortedByTimeMillis(): LiveData<List<Run>>
+
+    @Query("SELECT * FROM running_table ORDER BY caloriesBurned DESC")
+    fun getAllRunsSortedCaloriesBurned(): LiveData<List<Run>>
+
+    @Query("SELECT * FROM running_table ORDER BY avgSpeedInKMH DESC")
+    fun getAllRunsSortedByAvgSpeed(): LiveData<List<Run>>
+
+    @Query("SELECT * FROM running_table ORDER BY distanceInMeters DESC")
+    fun getAllRunsSortedByDistance(): LiveData<List<Run>>
+}
